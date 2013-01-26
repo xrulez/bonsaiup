f655a41b9795c279a326895e71a0d9894c38aa94
xrulez <xrulez@mightyfish.(none)>
2013-01-26 12:19:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillexchange::Application.config.secret_token = '748cdcb64f810b7bd7c492d0fdbbf688814812ce8bce4fdbff31e208b8aeb446723b789808a61a13a2b6e181ac7168be756db8bae4b753b48025062a16e7b34b'
